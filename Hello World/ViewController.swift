80537d205b83c8f0aa311b876751f43d9d1743e5
Anastasia Kozlova <kozlova.sia@yandex.ru>
2024-04-16 21:27:30 +0200

    Initial Commit

---
+++ Hello World/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Hello World
+//
+//  Created by Анастасия Козлова on 16.04.2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
